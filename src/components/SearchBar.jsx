97056577c4d44627e7bc66f3b4034e29a44ab237
Natalie Sokolova <nataliesklv@gmail.com>
2021-01-12 23:29:07 -0500

    changes state on input change

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,15 +5,24 @@ import { Form, Input, Button } from "antd";
 export default function SearchBar() {
   const [form] = Form.useForm();
   const [movie, setMovie] = useState("");
+
+  const handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+
+    setMovie(value);
+  };
+
   return (
     <div>
+      {console.log("MOVIE: ", movie)}
       <Form name="basic" initialValues={{ remember: true }}>
         <Form.Item
           label="Movie title"
           name="movieTitle"
           rules={[{ required: true, message: "Please input the movie title!" }]}
         >
-          <Input placeholder="movie title" />
+          <Input placeholder="movie title" onChange={handleInputChange} />
         </Form.Item>
 
         <Form.Item>
