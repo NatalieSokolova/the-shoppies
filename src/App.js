67df1498d3edbdbce8127baedb743281360c1262
Natalie Sokolova <nataliesklv@gmail.com>
2021-01-13 12:50:33 -0500

    creates a NominationsCard component and passes state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { useState } from "react";
 import SearchBar from "./components/SearchBar";
 import ResultsCard from "./components/ResultsCard";
+import NominationsCard from "./components/NominationsCard";
 import "./App.css";
 
 function App() {
   const [movieList, setMovieList] = useState("");
-  const [nominatedMovie, setNominatedMovie] = useState("");
+  const [nominatedMovie, setNominatedMovie] = useState(false);
 
   console.log("LIST: ", movieList);
+  console.log("nominatedMovie: ", nominatedMovie);
 
   return (
     <div>
       <SearchBar movieList={movieList} setMovieList={setMovieList} />
-      <ResultsCard movieList={movieList} />
+      <ResultsCard
+        movieList={movieList}
+        nominatedMovie={nominatedMovie}
+        setNominatedMovie={setNominatedMovie}
+      />
+      <NominationsCard
+        movieList={movieList}
+        nominatedMovie={nominatedMovie}
+        setNominatedMovie={setNominatedMovie}
+      />
     </div>
   );
 }
---
+++ src/components/NominationsCard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NominationsCard({ movieList }) {
+  console.log("LIST FROM RESULTS: ", movieList);
+  return (
+    <div>
+      <div>These are my nominations</div>
+    </div>
+  );
+}
