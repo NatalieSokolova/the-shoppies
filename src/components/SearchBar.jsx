e55bff01a92ff9128eeb0a17e936ee763992a418
Natalie Sokolova <nataliesklv@gmail.com>
2021-01-12 22:34:06 -0500

    uses antd form

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import { Form, Input, Button } from "antd";
 
 export default function SearchBar() {
-  return <div>I'm a search bar!</div>;
+  return (
+    <div>
+      <Form
+        name="basic"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: "Please input the movie title!" }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Search
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 }
