247c1ef9d24fdb8650cf110072c2ef5bd95efd83
Natalie Sokolova <nataliesklv@gmail.com>
2021-01-15 17:34:17 -0500

    centers link

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import "./Home.css";
 
 export default function Home() {
   return (
-    <div id="homePage">
-      <div id="logo">Logo banner goes here</div>
-      <a href="#searchBar">Nominate Movies</a>
+    <div className="homePage">
+      <div className="logo">Logo banner goes here</div>
+      <div className="linkToSearch">
+        <a href="#searchBar">Nominate Movies</a>
+      </div>
     </div>
   );
 }
